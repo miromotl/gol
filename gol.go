3d07fb36269bcac31e53365e8591cb49eb24110e
Miro Motl <miromotl@yahoo.com>
2015-09-09 19:39:14 +0200

    "Added command line parameters for pattern and dimension"

--- gol.go
+++ gol.go
@@ -17,6 +17,9 @@ package main
 import (
 	"github.com/spf13/cobra"
 	"fmt"
+	"strings"
+	"strconv"
+	"os"
 )
 
 // We are storing the cells (alive or dead) in a map. The keys are the Cartesian
@@ -109,14 +112,15 @@ func (world World) Tick() World {
 }
 
 // gnuplotHeader prints the header for gnuplot
-func gnuplotHeader() {
-	fmt.Println("unset key; set xrange[-50:50]")
-	fmt.Println("set yrange[-50:50]")
+func gnuplotHeader(d int) {
+	fmt.Printf("unset key; set xrange[-%[1]d:%[1]d]\n", d/2)
+	fmt.Printf("set yrange[-%[1]d:%[1]d]\n", d/2)
+	fmt.Println("set style line 1 lc rgb '#0060ad' pt 7")
 }
 
 // gnuplotWorld prints the coordinates of the cells in the world
 func gnuplotWorld(world World) {
-	fmt.Println("plot '-' with points pointtype 6")
+	fmt.Println("plot '-' with points ls 1")
 
 	for coord := range world {
 		fmt.Printf("%d, %d\n", coord.x, coord.y)
@@ -127,6 +131,9 @@ func gnuplotWorld(world World) {
 
 func main() {
 	ticks := 10  // how many ticks the game is running
+	pattern := "1,0;0,1;1,1;1,2;2,2"  // initial pattern of life cells
+	d := 100 // width and height of gnuplot window: -d/2 <= x <= d/2 and -d/2 <= y <= d/2
+
 
 	// Cobra configuration
 	var rootCmd = &cobra.Command{
@@ -141,28 +148,55 @@ its initial state, requiring no further input. One interacts with the Game of
 Life by creating an initial configuration and observing how it evolves or, for 
 advanced players, by creating patterns with particular properties.`,
 		Run: func(cmd *cobra.Command, args []string) {
-			gol(ticks)
+			gol(d, pattern, ticks)
 		},
 	}
 
 	rootCmd.Flags().IntVarP(&ticks, "ticks", "t", 10, "ticks to run the game")
+	rootCmd.Flags().StringVarP(&pattern, "pattern", "p", "1,0;0,1;1,1;1,2;2,2", "initial pattern of live cells")
+	rootCmd.Flags().IntVarP(&d, "dimension", "d", 50, "width and height of gnuplot window")
 
 	rootCmd.Execute()
 }
 
-func gol(ticks int) {
+func gol(d int, pattern string, ticks int) {
 	// The world
 	var world World
 	world = make(World)
-	
+
+	/*
 	// Define a starting world: the r-Pentomino
 	world[Coord{1, 0}] = Cell{true, 0}
 	world[Coord{0, 1}] = Cell{true, 0}
 	world[Coord{1, 1}] = Cell{true, 0}
 	world[Coord{1, 2}] = Cell{true, 0}
 	world[Coord{2, 2}] = Cell{true, 0}
-	
-	gnuplotHeader()
+	 */
+
+	// The starting configuratio in the string cells
+	coordinates := strings.Split(pattern, ";")
+
+	for _, xy := range coordinates {
+		c := strings.Split(xy, ",")
+		if len(c) < 2 {
+			fmt.Println("pattern error:", pattern, ":", xy)
+			os.Exit(1)
+		}
+		x, err := strconv.Atoi(c[0])
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		y, err := strconv.Atoi(c[1])
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		world[Coord{x, y}] = Cell{true, 0}
+	}
+
+	gnuplotHeader(d)
 
 	gnuplotWorld(world)
 	
